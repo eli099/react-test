5166972ba90e6e21cf94f85e20a9dc096723ae73
Eliott McKenzie <e4991m@gmail.com>
2023-07-26 23:49:11 +0100

    added try/catch to get request function

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,20 @@
-import { useEffect } from 'react'
+import React, { useEffect } from 'react'
 import axios from 'axios'
 
 const App = () => {
   useEffect(() => {
     const getData = async () => {
-      const { data } = await axios.get('https://api.coincap.io/v2/rates') // * <-- replace with your endpoint
-      console.log(data)
+    
+      try {
+        const { data } = await axios.get('https://api.coincap.io/v2/rates') // * <-- replace with your endpoint
+        console.log(data)
+      } catch (error) {
+        console.log(error)
+      }
+
     }
     getData()
-  })
+  }, []) // empty array in dependencies causes the useEffect to only trigger once on the first render
 
   return <h1>Hello World</h1>
 }
